a7f7c71073cc3677f81194a4557abf098233e77c
Sarinya Won <sarinya.won@gmail.com>
2020-10-22 15:24:13 +0700

    Add Portfolios folder & Job Board folder

---
+++ portfolios/job-board/precache-manifest.3acd55c78f9f2d80d42307bc33ad7bcd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07651814f98c875b1af7d82510e41273",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c9fac45c9b09f149d1c",
+    "url": "/static/css/main.3774c839.chunk.css"
+  },
+  {
+    "revision": "740b39cf8d38e74122c0",
+    "url": "/static/js/2.f84521ca.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/static/js/2.f84521ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c9fac45c9b09f149d1c",
+    "url": "/static/js/main.915f2149.chunk.js"
+  },
+  {
+    "revision": "bf9fda22f05c083606a6",
+    "url": "/static/js/runtime-main.ccf115f7.js"
+  }
+]);
