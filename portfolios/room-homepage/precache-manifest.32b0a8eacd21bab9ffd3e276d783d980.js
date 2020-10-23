28ecc12261122b5d22a9efea9b8cc8d4419fa401
Sarinya Won <sarinya.won@gmail.com>
2020-10-23 17:01:05 +0700

    Add Room Homepage Project

---
+++ portfolios/room-homepage/precache-manifest.32b0a8eacd21bab9ffd3e276d783d980.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0f29dda50b82ed2542b95cb9de2dead",
+    "url": "/portfolios/room-homepage/index.html"
+  },
+  {
+    "revision": "c0a043db39b06d6e3c73",
+    "url": "/portfolios/room-homepage/static/css/main.1850d5b9.chunk.css"
+  },
+  {
+    "revision": "26746beee26b507e2a0b",
+    "url": "/portfolios/room-homepage/static/js/2.0ec77ded.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/portfolios/room-homepage/static/js/2.0ec77ded.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0a043db39b06d6e3c73",
+    "url": "/portfolios/room-homepage/static/js/main.dcd4901a.chunk.js"
+  },
+  {
+    "revision": "d2ec3adc842a2e9c69a4",
+    "url": "/portfolios/room-homepage/static/js/runtime-main.8bda1750.js"
+  }
+]);
